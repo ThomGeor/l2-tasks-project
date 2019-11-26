6f67a10b8e967b34eb638bfe52e2aa8e37c365eb
Malo Grall <malo.grall@gmail.com>
2019-11-26 11:40:12 +0100

    Commented all issues

--- src/reseauEchange/Admin.java
+++ src/reseauEchange/Admin.java
@@ -2,19 +2,25 @@ package reseauEchange;
 
 import java.util.ArrayList; 
 
+/** TODO
+ * Separate network and admin
+ */
 public class Admin extends Member{
 	private ArrayList<Member> network;
-	private String nameOfNetwork; 
+  private String nameOfNetwork; 
+  private Network network;
 	
 	public Admin(int wallet, String name, SocialClass socialClass){
 		super(wallet, name, socialClass);
 		this.network = new ArrayList<Member>(); 
 	}
-	
+  
+  // Create calss network
 	public void createNetwork(String name) {
-		this.nameOfNetwork = name; 
+		this.network = new Network()
 	}
-	
+  
+  // 2 methods : with or without wallet amount
 	public void createMember(String name, SocialClass socialClass, double wallet) {
 		network.add(network.size(), new Member(wallet, name, socialClass)); 
 	}
--- src/reseauEchange/Member.java
+++ src/reseauEchange/Member.java
@@ -3,21 +3,26 @@ package reseauEchange;
 import java.util.ArrayList;
 
 public class Member {
+  // list of networks
 	private double wallet;  
 	private String name; 
 	private SocialClass socialClass; 
 	private ArrayList<Service> services; 
-	
+  
+  // 2 constructers : 1 with amount init wallet, 1 default 0
 	public Member(double wallet, String name, SocialClass socialClass) {
 		this.services = new ArrayList<Service>(); 
 		this.wallet = wallet; 
 		this.name = name; 
 		this.socialClass = socialClass; 
 	}
-	
+  
+  // Add service
 	public void newService() {
 		
-	}
+  }
+  
+  // Take care of amount <0 -> can't create task ??
 	public void increaseWallet() {
 		wallet += services.get().
 	}

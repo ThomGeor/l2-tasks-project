5ff68c5a69ed0b2c2cba7ba9c5783fa2b8665d4f
Malo Grall <malo.grall@gmail.com>
2019-12-11 18:02:14 +0100

    Started to work on Service and Task

--- src/main/java/network/Task.java
+++ src/main/java/network/Task.java
@@ -3,32 +3,64 @@ package network;
 import java.util.ArrayList;
 
 public class Task {
-	private Service service; 
-	private Member beneficiary;
+    private Service service;
+    private Member beneficiary;
 	private ArrayList<Member> participants;
-	private double duration;
-	private int cost; // Save the cost if the task is executed
-	private boolean volunteer;
-	private boolean finished;
-	
-	public Task (Service service, Member beneficiary, ArrayList<Member> participants, double duration, boolean volunteer) {
-		this.service        = service; 
-		this.beneficiary    = beneficiary; 
-		this.participants   = participants;
-		this.duration       = duration;
-		this.volunteer      = volunteer;
-		this.finished       = false;
-	}
-  
-  // Return cost of the task according to beneficiary's class, duration and number of participants
-  // If finished doesn't recalculate
-	public int getCost() {
-		return (volunteer) ? 0 : Math.ceil(this.beneficiary.getSocialClass().calcPrice(this.duration * this.participants.size()));
-  }
-  
-  // Executes the task, pay the participants and debits the beneficiary
-  // Change status
-  public void execute(){
+    private double duration;
+    private int cost; // Save the cost if the task is executed
+    private boolean volunteer;
+    private boolean finished; // The admin can finish it when participants are found
 
-  }
+    public Task(Service service, Member beneficiary, ArrayList<Member> participants, double duration, boolean volunteer) {
+        this.service = service;
+        this.beneficiary = beneficiary;
+        this.participants = participants;
+        this.duration = duration;
+        this.volunteer = volunteer;
+        this.finished = false;
+    }
+
+    // Return cost of the task according to beneficiary's class, duration and number of participants
+    // If finished doesn't recalculate
+    public int getCost() {
+        return (volunteer) ? 0 : this.beneficiary.getSocialClass().calc((int)Math.ceil(this.duration * this.participants.size()));
+    }
+
+	public Service getService() {
+		return service;
+	}
+
+	public Member getBeneficiary() {
+		return beneficiary;
+	}
+
+	public ArrayList<Member> getParticipants() {
+		return participants;
+	}
+
+	public double getDuration() {
+		return duration;
+	}
+
+	public boolean isVolunteer() {
+		return volunteer;
+	}
+
+	public boolean isFinished() {
+		return finished;
+	}
+
+	/**
+	 * Find all participants in the Beneficiary's Network
+	 * */
+	public void findParticipants(){
+		
+	}
+
+    // Executes the task, pay the participants and debits the beneficiary
+	// Only done by admin
+    // Change status : finished = true
+    public void execute() {
+
+    }
 }
